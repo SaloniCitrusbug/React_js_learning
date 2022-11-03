9d7a221cd410e51f70830a94f7ae9703e0bab096
saloni modi <saloni.citrusbug@gmail.com>
2022-11-03 17:56:43 +0530

    3-11

--- reducer/src/Compo/ComC.js
+++ reducer/src/Compo/ComC.js
@@ -10,3 +10,65 @@ function ComC() {
 }
 
 export default ComC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useReducer } from 'react'
+
+// const initialState = {
+//     firstCounter: 0
+// }
+// const reducer = (state, action) => {
+//     switch (action.type) {
+//         case 'increment':
+//             return { firstCounter: state.firstCounter + 1 }
+//         case 'decrement':
+//             return { firstCounter: state.firstCounter - 1 }
+//         case 'reset':
+//             return initialState
+//         default:
+//             return state
+//     }
+// }
+
+// function ComA() {
+
+//     const [count, dispatch] = useReducer(reducer, initialState)
+
+//     return (
+//         <div>
+//             Count : {count.firstCounter}
+
+//             <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+//             <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+//             <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+
+//         </div>
+//     )
+// }
+
+// export default ComA
